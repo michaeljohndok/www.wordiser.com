d2afc1379c0d5ec35add59c27a2f26ffa1449ede
guoquan mai <nacheal@gmail.com>
2020-02-27 22:29:31 +0800

    uploaded..

---
+++ assets/images/js/demo.js
@@ -0,0 +1,249 @@
+/**
+ * Do the clicking stuff
+ *
+ */
+
+(function() {
+  var cards = document.querySelectorAll(".card.effect__click");
+  for ( var i  = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    clickListener( card );
+  }
+
+  function clickListener(card) {
+    card.addEventListener( "click", function() {
+      var c = this.classList;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+    });
+  }
+})();
+
+/**
+ * Do the random stuff
+ *
+ */
+
+(function() {
+
+  // cache vars
+  var cards = document.querySelectorAll(".card.effect__random");
+  var timeMin = 1;
+  var timeMax = 1;
+  var timeouts = [];
+
+  // loop through cards
+  for ( var i = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    var cardID = card.getAttribute("data-id");
+    var id = "timeoutID" + cardID;
+    var time = randomNum( timeMin, timeMax ) * 2000;
+    cardsTimeout( id, time, card );
+  }
+
+  // timeout listener
+  function cardsTimeout( id, time, card ) {
+    if (id in timeouts) {
+      clearTimeout(timeouts[id]);
+    }
+    timeouts[id] = setTimeout( function() {
+      var c = card.classList;
+      var newTime = randomNum( timeMin, timeMax ) * 8000;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+      cardsTimeout( id, newTime, card );
+    }, time );
+  }
+
+  // random number generator given min and max
+  function randomNum( min, max ) {
+    return Math.random() * (max - min) + min;
+  }
+
+})();
+
+(function() {
+
+  // cache vars
+  var cards = document.querySelectorAll(".card.effect__random1");
+  var timeMin = 2;
+  var timeMax = 2;
+  var timeouts = [];
+
+  // loop through cards
+  for ( var i = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    var cardID = card.getAttribute("data-id");
+    var id = "timeoutID" + cardID;
+    var time = randomNum( timeMin, timeMax ) * 2000;
+    cardsTimeout( id, time, card );
+  }
+
+  // timeout listener
+  function cardsTimeout( id, time, card ) {
+    if (id in timeouts) {
+      clearTimeout(timeouts[id]);
+    }
+    timeouts[id] = setTimeout( function() {
+      var c = card.classList;
+      var newTime = randomNum( timeMin, timeMax ) * 6000;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+      cardsTimeout( id, newTime, card );
+    }, time );
+  }
+
+  // random number generator given min and max
+  function randomNum( min, max ) {
+    return Math.random() * (max - min) + min;
+  }
+
+})();
+
+(function() {
+
+  // cache vars
+  var cards = document.querySelectorAll(".card.effect__random2");
+  var timeMin = 3;
+  var timeMax = 3;
+  var timeouts = [];
+
+  // loop through cards
+  for ( var i = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    var cardID = card.getAttribute("data-id");
+    var id = "timeoutID" + cardID;
+    var time = randomNum( timeMin, timeMax ) * 2000;
+    cardsTimeout( id, time, card );
+  }
+
+  // timeout listener
+  function cardsTimeout( id, time, card ) {
+    if (id in timeouts) {
+      clearTimeout(timeouts[id]);
+    }
+    timeouts[id] = setTimeout( function() {
+      var c = card.classList;
+      var newTime = randomNum( timeMin, timeMax ) * 5000;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+      cardsTimeout( id, newTime, card );
+    }, time );
+  }
+
+  // random number generator given min and max
+  function randomNum( min, max ) {
+    return Math.random() * (max - min) + min;
+  }
+
+})();
+
+(function() {
+
+  // cache vars
+  var cards = document.querySelectorAll(".card.effect__random3");
+  var timeMin = 4;
+  var timeMax = 4;
+  var timeouts = [];
+
+  // loop through cards
+  for ( var i = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    var cardID = card.getAttribute("data-id");
+    var id = "timeoutID" + cardID;
+    var time = randomNum( timeMin, timeMax ) * 2000;
+    cardsTimeout( id, time, card );
+  }
+
+  // timeout listener
+  function cardsTimeout( id, time, card ) {
+    if (id in timeouts) {
+      clearTimeout(timeouts[id]);
+    }
+    timeouts[id] = setTimeout( function() {
+      var c = card.classList;
+      var newTime = randomNum( timeMin, timeMax ) * 4000;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+      cardsTimeout( id, newTime, card );
+    }, time );
+  }
+
+  // random number generator given min and max
+  function randomNum( min, max ) {
+    return Math.random() * (max - min) + min;
+  }
+
+})();
+
+
+(function() {
+
+  // cache vars
+  var cards = document.querySelectorAll(".card.effect__random4");
+  var timeMin = 5;
+  var timeMax = 5;
+  var timeouts = [];
+
+  // loop through cards
+  for ( var i = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    var cardID = card.getAttribute("data-id");
+    var id = "timeoutID" + cardID;
+    var time = randomNum( timeMin, timeMax ) * 2000;
+    cardsTimeout( id, time, card );
+  }
+
+  // timeout listener
+  function cardsTimeout( id, time, card ) {
+    if (id in timeouts) {
+      clearTimeout(timeouts[id]);
+    }
+    timeouts[id] = setTimeout( function() {
+      var c = card.classList;
+      var newTime = randomNum( timeMin, timeMax ) * 3000;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+      cardsTimeout( id, newTime, card );
+    }, time );
+  }
+
+  // random number generator given min and max
+  function randomNum( min, max ) {
+    return Math.random() * (max - min) + min;
+  }
+
+})();
+
+
+(function() {
+
+  // cache vars
+  var cards = document.querySelectorAll(".card.effect__random5");
+  var timeMin = 6;
+  var timeMax = 6;
+  var timeouts = [];
+
+  // loop through cards
+  for ( var i = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    var cardID = card.getAttribute("data-id");
+    var id = "timeoutID" + cardID;
+    var time = randomNum( timeMin, timeMax ) * 2000;
+    cardsTimeout( id, time, card );
+  }
+
+  // timeout listener
+  function cardsTimeout( id, time, card ) {
+    if (id in timeouts) {
+      clearTimeout(timeouts[id]);
+    }
+    timeouts[id] = setTimeout( function() {
+      var c = card.classList;
+      var newTime = randomNum( timeMin, timeMax ) * 2000;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+      cardsTimeout( id, newTime, card );
+    }, time );
+  }
+
+  // random number generator given min and max
+  function randomNum( min, max ) {
+    return Math.random() * (max - min) + min;
+  }
+
+})();
+
